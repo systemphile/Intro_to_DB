de4febeacc68c1ba8829d6b0ebd805031144ad07
systemphile <systemphile@gmail.com>
2025-06-29 17:08:17 +0300

    Let's Build Your Database

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", password="1234")
+mycursor = mydb.cursor()
+
+try:
+    mycursor.execute("CREATE DATABASE alx_book_store")
+except:
+    print("Failed to connect to the Database")
+else:
+    print("Database 'alx_book_store' created successfully!")
+finally:
+    mycursor.close()
+    mydb.close()
