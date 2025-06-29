fb06b7abf8f1c79b6a44ef943c6cb04910b4b422
systemphile <systemphile@gmail.com>
2025-06-29 16:44:11 +0300

    Database for Your Dream Online Bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -76,10 +76,10 @@ DROP TABLE IF EXISTS Customers;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE Customers (
-  `customer_id` int NOT NULL,
-  `customer_name` varchar(215) DEFAULT NULL,
-  `email` varchar(215) DEFAULT NULL,
-  `address` text,
+  `customer_id ` INT NOT NULL,
+  `customer_name` VARCHAR(215) DEFAULT NULL,
+  `email` VARCHAR(215) DEFAULT NULL,
+  `address` TEXT,
   PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
