882381e157fbdbb8be4614d65dae37af6bf507a9
systemphile <systemphile@gmail.com>
2025-06-29 17:09:03 +0300

    Let's Build Your Database

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,7 +4,7 @@ mydb = mysql.connector.connect(host="localhost", user="root", password="1234")
 mycursor = mydb.cursor()
 
 try:
-    mycursor.execute("CREATE DATABASE alx_book_store")
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 except:
     print("Failed to connect to the Database")
 else:
